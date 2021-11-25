e7d2fed5e16aeca9bdf972d5e307c6efa362510a
ikaronauta <jaortiz0496@misena.edu.co>
2021-11-25 08:59:00 -0500

    Update seeder Clientes

--- database/seeders/ClienteSeeder.php
+++ database/seeders/ClienteSeeder.php
@@ -14,6 +14,126 @@ class ClienteSeeder extends Seeder
      */
     public function run()
     {
+        //Cliente 1
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 234233182;
+        $cliente->nombre_cliente = 'Fabrica de Cajas 1A';
+        $cliente->municipio_cliente = 1;
+        $cliente->telefono_cliente = '8905432';
+        $cliente->direccion_cliente = 'Cra 45 # 76-22';
+        $cliente->correo_cliente = 'contacto@1a.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 2
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 43552637;
+        $cliente->nombre_cliente = 'Regalos y Más';
+        $cliente->municipio_cliente = 1;
+        $cliente->telefono_cliente = '8785342';
+        $cliente->direccion_cliente = 'Cl 26 # 65-154';
+        $cliente->correo_cliente = 'contacto@regalos.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 3
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 32272993;
+        $cliente->nombre_cliente = 'Transportadora Universal';
+        $cliente->municipio_cliente = 6;
+        $cliente->telefono_cliente = '8653425';
+        $cliente->direccion_cliente = 'Cl 45 # 12-26';
+        $cliente->correo_cliente = 'contacto@universal.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 4
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 32272993;
+        $cliente->nombre_cliente = 'Cargo Plus';
+        $cliente->municipio_cliente = 6;
+        $cliente->telefono_cliente = '8653425';
+        $cliente->direccion_cliente = 'Cl 88 # 126-26';
+        $cliente->correo_cliente = 'contacto@plus.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 5
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 32272993;
+        $cliente->nombre_cliente = 'Multilujos';
+        $cliente->municipio_cliente = 3;
+        $cliente->telefono_cliente = '342342345';
+        $cliente->direccion_cliente = 'Cr 122 # 34-3';
+        $cliente->correo_cliente = 'contacto@Multilujos.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 6
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 32272993;
+        $cliente->nombre_cliente = 'Autopartes La Estrella';
+        $cliente->municipio_cliente = 1;
+        $cliente->telefono_cliente = '5435344';
+        $cliente->direccion_cliente = 'Cr 23 # 64-32';
+        $cliente->correo_cliente = 'contacto@estrella.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 7
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 32272993;
+        $cliente->nombre_cliente = 'Recda';
+        $cliente->municipio_cliente = 5;
+        $cliente->telefono_cliente = '4353454';
+        $cliente->direccion_cliente = 'Cl 12 # 34-100';
+        $cliente->correo_cliente = 'contacto@recda.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 8
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 32272993;
+        $cliente->nombre_cliente = 'Importadora Superior';
+        $cliente->municipio_cliente = 6;
+        $cliente->telefono_cliente = '3424324';
+        $cliente->direccion_cliente = 'Cl 87 # 12-14';
+        $cliente->correo_cliente = 'contacto@superior.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 9
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 32272993;
+        $cliente->nombre_cliente = 'Todo Partes Alex';
+        $cliente->municipio_cliente = 6;
+        $cliente->telefono_cliente = '654567';
+        $cliente->direccion_cliente = 'Cr 6 # 121-23';
+        $cliente->correo_cliente = 'contacto@todopartes.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
+        //Cliente 10
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 32272993;
+        $cliente->nombre_cliente = 'Todo Partes Alex';
+        $cliente->municipio_cliente = 6;
+        $cliente->telefono_cliente = '654567';
+        $cliente->direccion_cliente = 'Cr 6 # 121-23';
+        $cliente->correo_cliente = 'contacto@todopartes.com';
+        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->save();
+
         //Bellota
         $cliente = new Cliente();
         $cliente->tipo_documento = 2;
@@ -21,9 +141,9 @@ class ClienteSeeder extends Seeder
         $cliente->nombre_cliente = 'Bellota';
         $cliente->municipio_cliente = 1;
         $cliente->telefono_cliente = '8905432';
-        $cliente->direccion_cliente = 'Parque Industrial Juanchito Terraza N°8';
+        $cliente->direccion_cliente = 'PQ Industrial Juanchito Terraza N°8';
         $cliente->correo_cliente = 'contacto@bellota.com';
-        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->logo_cliente = 'bellota.png';
         $cliente->save();
 
         //Casa Luker
@@ -33,9 +153,9 @@ class ClienteSeeder extends Seeder
         $cliente->nombre_cliente = 'Casa Luker';
         $cliente->municipio_cliente = 1;
         $cliente->telefono_cliente = '8785342';
-        $cliente->direccion_cliente = 'CasaLuker, Carrera 23 N° 64B - 33 Edificio Centro de Negocios Siglo XXI';
+        $cliente->direccion_cliente = 'Cr 23 N° 64B-33 Edificio Centro de Negocios Siglo XXI';
         $cliente->correo_cliente = 'contacto@casaluker.com';
-        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->logo_cliente = 'luker.png';
         $cliente->save();
 
         //Saferbo
@@ -45,9 +165,33 @@ class ClienteSeeder extends Seeder
         $cliente->nombre_cliente = 'Saferbo';
         $cliente->municipio_cliente = 6;
         $cliente->telefono_cliente = '8653425';
-        $cliente->direccion_cliente = 'Calle 9 #1W-12, La Badea';
+        $cliente->direccion_cliente = 'Cl 9 #1W-12, La Badea';
         $cliente->correo_cliente = 'contacto@saferbo.com';
-        $cliente->logo_cliente = 'default-circulo.png';
+        $cliente->logo_cliente = 'saferbo.jpg';
+        $cliente->save();
+
+        //Cafe Quindio
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 654645645;
+        $cliente->nombre_cliente = 'Cafe Quindio';
+        $cliente->municipio_cliente = 3;
+        $cliente->telefono_cliente = '43535435';
+        $cliente->direccion_cliente = 'cl 110 # 34-26';
+        $cliente->correo_cliente = 'contacto@cafequindio.com';
+        $cliente->logo_cliente = 'saferbo.jpg';
+        $cliente->save();
+
+        //Club Manizales
+        $cliente = new Cliente();
+        $cliente->tipo_documento = 2;
+        $cliente->documento_cliente = 654645645;
+        $cliente->nombre_cliente = 'Club Manizales';
+        $cliente->municipio_cliente = 3;
+        $cliente->telefono_cliente = '43535435';
+        $cliente->direccion_cliente = 'cl 110 # 34-26';
+        $cliente->correo_cliente = 'contacto@clubmanizales.com';
+        $cliente->logo_cliente = 'clubmanizales.jpg';
         $cliente->save();
     }
 }
