b9a02b8600b16f2e700a0336f359db39ed31a11d
ikaronauta <jaortiz0496@misena.edu.co>
2021-11-25 08:02:03 -0500

    update alt view-show clientes

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -42,7 +42,7 @@
     <div class="shadow-lg p-3 mb-3 bg-body rounded border border-2 border-success">
         <div class="row">
             <div class="col-sm-4">
-                <img src="{{ asset('img/clientes/' . $cliente->logo_cliente) }}" alt="Logo Cliente" style="width: 250px">
+                <img src="{{ asset('img/clientes/' . $cliente->logo_cliente) }}" alt="Logo {{ $cliente->nombre_cliente }}" style="width: 250px">
             </div>
         
             <div class="col-sm-8">
